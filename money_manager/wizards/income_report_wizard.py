eeea7b19331ca9904d529cc217211bda6fd5c3d6
honguyen10 <tranhonguyen1999@gmail.com>
2022-12-01 14:35:16 +0700

    [ADD] Add date range to wizard to show report

--- money_manager/wizards/income_report_wizard.py
+++ money_manager/wizards/income_report_wizard.py
@@ -9,9 +9,16 @@ class IncomeReportWizard(models.TransientModel):
     partner_id = fields.Many2one('res.partner', string='User Name')
     account_id = fields.Many2one('money.account', string='Account',
         domain="[('partner_id', '=', partner_id)]")
+    date_from = fields.Date(string='From')
+    date_to = fields.Date(string='To')
 
     def select_income_report_account(self):
-        domain = [('partner_id', '=', self.partner_id.id), ('account_id', '=', self.account_id.id)]
+        domain = [
+            ('partner_id', '=', self.partner_id.id),
+            ('account_id', '=', self.account_id.id),
+            ('date', '>=', self.date_from),
+            ('date', '<=', self.date_to)
+            ]
         return {
             'name': 'Income Report',
             'view_mode': 'graph',
