224ca83021db441168a7d53913f0c87048c7c3a0
honguyen10 <tranhonguyen1999@gmail.com>
2022-12-16 15:53:51 +0700

    Rename record transaction remind

--- money_manager/models/res_partner.py
+++ money_manager/models/res_partner.py
@@ -4,6 +4,6 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    record_transaction_reminder = fields.Boolean(
-        string='Record Transaction Reminder')
+    record_transaction_remind = fields.Boolean(
+        string='Record Transaction Remind')
     report_notification = fields.Boolean(string='Report Notification')
